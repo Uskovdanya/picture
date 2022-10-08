65d23f6200bef88b056954be2e1c74c0ca432882
Daniil <uskov.danya@gmail.com>
2022-10-08 07:47:01 +0300

    first commit

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,9 @@
+const sliders = (slides, dir, prev, next) => {
+  let slideIndex = 1;
+  const items = document.querySelectorAll(slides),
+        prevBtn = document.querySelector(prev),
+        nextBtn = document.querySelector(next);
+
+};
+
+export default sliders;
