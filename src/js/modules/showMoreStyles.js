fcc0979e8e62bec2d44ddfe0d6974180bb980b47
Daniil <uskov.danya@gmail.com>
2022-10-11 07:11:39 +0300

    fifth commit

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,61 @@
+import {getResource} from './services/requests';
+
+
+const showMoreStyles = (trigger, wrapper) => {
+  const btn = document.querySelector(trigger);
+
+
+        // способ с классами
+  //cards.forEach(card => {
+  //  card.classList.add('animated', 'fadeInUp');
+  //});
+
+  //btn.addEventListener('click', () => {
+  //  cards.forEach(card => {
+  //    card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+  //    card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+  //  });
+  //  btn.style.display = 'none';
+  //});
+
+  // способ с подгрузкой данных с сервера
+  function errorMessage() {
+    let statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    statusMessage.textContent = "Что-то пошло не так...";
+    statusMessage.style.cssText = 'text-align: center; font-size: 250%; color: red';
+    document.querySelector(wrapper).appendChild(statusMessage);
+  }
+
+  btn.addEventListener('click', function() {
+    getResource('assets/db.json')
+      .then(res => createCards(res.styles))
+      .catch(error => errorMessage());
+    
+    this.remove();
+  });
+  //у стрелочной функции в обработчиках не будет ссылаться на тот элемн на котором произошло событие. У него нет this/ чтобы использовать его нужно сделать обычную функцию 
+
+  
+
+  function createCards(response) {
+    response.forEach(({src, title, link}) => {
+      let card = document.createElement('div');
+
+      card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+      card.innerHTML = `
+        <div class="styles-block">
+          <img src=${src} alt ="style">
+          <h4>${title}</h4>
+          <a href="${link}">Подробнее</a>
+        </div>
+      `;
+      
+      document.querySelector(wrapper).appendChild(card);
+    });
+  }
+
+};
+
+export default showMoreStyles;
